0a9f843d9031dcd10875433ed38aef72d81be428
allen.wu <allen.wu@goodlinker.io>
2023-04-10 17:12:03 +0800

    fix bug

--- laravel_chat/app/resources/views/dialogue/index.blade.php
+++ laravel_chat/app/resources/views/dialogue/index.blade.php
@@ -571,7 +571,7 @@
                                     <div  style="margin: 0;">
                                         <?php
                                             list($fiel, $type)= explode(".", $m->file);
-                                            if ( (stripos($type, "png") !== false) ) || (stripos($type, "jpg") !== false))  || (stripos($type, "tiff") !== false)) || (stripos($type, "gif") !== false) ) || (stripos($type, "jpeg")!== false) )){
+                                            if ( (stripos($type, "png") !== false)|| (stripos($type, "jpg") !== false)  || (stripos($type, "tiff") !== false) || (stripos($type, "gif") !== false) || (stripos($type, "jpeg")!== false) ){
                                          ?>
                                             <img src="/file/{{ $m->file}}" alt="" class="initImg containImg">
                                         <?php   } else { ?>
